7485a7f1154d1be4edddd89641bbeb96ee0a68c0
yishuangxi <2546618112@qq.com>
2016-09-17 23:55:17 +0800

    go package test

---
+++ src/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"mymath"
+)
+
+func main(){
+	fmt.Println("hello world")
+	res := mymath.Add(1, 2)
+	fmt.Println("res: ", res)
+}
+
---
+++ src/mymath/add.go
@@ -0,0 +1,5 @@
+package mymath
+
+func Add(x, y float64) float64 {
+	return x + y
+}
